1ba9dd8adcb47d73ed8fa32051c1ec33e0ae7cac
srirampoondichinappa <2k6sriram@gmail.com>
2018-01-17 09:56:02 -0700

    Adds Navigator and view file

---
+++ quizCards/utils/navigator.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Text, View, Platform } from 'react-native';
+import { lightPurp, white } from '../styles/colors';
